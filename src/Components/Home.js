0aaa6cea70e5fe1bf9c698e2cf04cee1f6c3ab26
surafeljs <surafel.node@gmail.com>
2025-06-02 10:54:52 -0700

    axios  id fetch

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,8 +1,36 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 const Home = () => {
   const [data, setData] = useState([]);
+  const [error, setError] = useState([]);
+
   const API = "https://fakestoreapi.com/products";
-  return <></>;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(API);
+        setData(response.data);
+      } catch (err) {
+        setError("Data not fetched");
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      {data.map((items, index) => {
+        return (
+          <div key={index}>
+            <ul>
+              <li>{items.id} </li>
+            </ul>
+          </div>
+        );
+      })}
+    </>
+  );
 };
 
 export default Home;
