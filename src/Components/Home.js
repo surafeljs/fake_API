ee3cb5d08828c656312f58aec6213e48d528e01f
surafeljs <surafel.node@gmail.com>
2025-06-03 12:51:19 -0700

    filter

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,17 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Button, FormControl, FormGroup, Form } from "react-bootstrap";
+import {
+  Button,
+  FormControl,
+  FormGroup,
+  Form,
+  FormSelect,
+} from "react-bootstrap";
 const Home = () => {
   const [data, setData] = useState([]);
   const [error, setError] = useState([]);
+  const [category, setCategoty] = useState("All");
+  const [search, setSearch] = useState(null);
 
   const API = "https://fakestoreapi.com/products";
 
@@ -18,26 +26,56 @@ const Home = () => {
     };
     fetchData();
   }, []);
-  const handler = (e) => {
-    e.preventDefault();
-    console.log("fff");
-  };
+  const categoryRemove = new Set(data.map((item) => item.category));
+
+  const filler = data.filter((item) => {
+    const itemTitle = item.title ? item.title.toLowerCase() : "";
+    const searchTerm = search ? search.toLowerCase() : "";
+
+    return (
+      category === "All" ||
+      item.category === category ||
+      itemTitle.includes(searchTerm)
+    );
+  });
+  console.log(filler);
+
   return (
     <>
-      <Form onSubmit={handler}>
-        <FormGroup className="flex flex-col justify-center  ">
-          <FormControl
-            className=" border rounded-lg border-neutral-500 "
-            type="text"
-            placeholder="Search ... "
-          ></FormControl>
-          <FormControl
-            className="bg-[#EEEEEE]"
-            type="submit"
-            placeholder="Search ... "
-          ></FormControl>
-        </FormGroup>
-      </Form>
+      <section className="bg-[#F9F7F7] h-screen">
+        <Form>
+          <FormGroup className=" flex gap-7 justify-center">
+            <FormControl
+              className=" w-96 mt-20 p-2 border rounded-lg border-neutral-500 placeholder: font-sans placeholder: text-xl "
+              type="text"
+              placeholder="Search ... "
+              onChange={(e) => setSearch(e.target.value)}
+            ></FormControl>
+            <FormSelect
+              className=" w-28 mt-20 p-2 border rounded-lg"
+              onChange={(e) => setCategoty(e.target.value)}
+            >
+              {/* <option value={"All"}>All</option>
+              <option value={"men's clothing"}>men's clothing</option>
+              <option value={"women's clothing"}>women's clothing</option>
+              <option value={"jewelery"}>jewelery</option>
+              <option value={"electronics"}>electronics</option> */}
+              {Array.from(categoryRemove).map((item, index) => (
+                <option key={index} value={item}>
+                  {item}
+                </option>
+              ))}
+            </FormSelect>
+            <FormControl
+              className="bg-[#EEEEEE] mt-20 w-40 text-lg font-sans hover:text-[#FFFF] transition-all duration-300 hover:bg-[#BBE1FA]
+            p-2 border rounded-lg"
+              type="submit"
+              value={"Search"}
+              placeholder="Search ... "
+            ></FormControl>
+          </FormGroup>
+        </Form>
+      </section>
     </>
   );
 };
